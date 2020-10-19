1e143a7fedeb71c756f383362fded4aca6e15989
acoudray1 <axel.hpm.coudray@gmail.com>
2020-10-19 11:15:59 +0200

    hook component update

--- src/components/HookComponent.jsx
+++ src/components/HookComponent.jsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 const HookComponent = (props) => {
+    const [mario, setMario] = useState(true);
     const [title, setTitle] = useState("Un titre bof");
 
-    useEffect(() => {
+    /*useEffect(() => {
         console.log("Je ne m'exécuterai qu'une seule et unique fois, lorsque le composant sera monté");
-    }, []);
+    }, []);*/
+
+    useEffect(() => {
+        console.log("Je m'exécuterai quand le composant sera monté et à chaque mise à jour du composant");
+    });
+
+    const toggleCharacter = () => {
+        setMario(!mario);
+    }
 
     const updateComposant = () => {
         setTitle("Un titre de fou");
@@ -14,6 +23,8 @@ const HookComponent = (props) => {
     return (
         <div className='HookComponent'>
             <h1>{title}</h1>
+            {mario ? <h3>It's me MARIOOOO</h3> : <h3>I'm Lord Bowser</h3>}
+            <button onClick={toggleCharacter}>Change character</button>
             <button onClick={updateComposant}>Update Composant</button>
         </div>
     );
